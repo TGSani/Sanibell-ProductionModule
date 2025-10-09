02a1d1711f95c72127604494eb809795f999db76
Thomas G <thomas.geeve@sanibell.nl>
2025-10-09 15:13:06 +0200

    Enhance user interface and functionality: update login page layout, implement order table in user index, and improve mock order data; add userIndex.css for styling.

--- Pages/User/Index.cshtml.cs
+++ Pages/User/Index.cshtml.cs
@@ -1,24 +1,31 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sanibell_ProductionModule.Repositories.Interfaces;
 
 namespace Sanibell_ProductionModule.Pages.User
 {
     [Authorize (Policy = "RequireProductionRole")]
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IOrderRepository _orders;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(IOrderRepository orders)
         {
-            _logger = logger;
+            _orders = orders;
         }
 
-        public void OnGet()
+        public IReadOnlyList<Models.Order>? Orders { get; private set; }
+        public async Task OnGetAsync()
         {
             // Zet de ViewData flags voor het weergeven van de knoppen in de layout
             ViewData["ShowBackButton"] = true;
             ViewData["ShowLogoutButton"] = false;
+
+            Orders = await _orders.GetOrdersAsync();
+
+
         }
     }
 
