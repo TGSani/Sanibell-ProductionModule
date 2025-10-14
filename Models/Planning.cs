de9e3d45292522305d3aa0747a6f8ab5e1f6b226
Thomas G <thomas.geeve@sanibell.nl>
2025-10-14 16:28:34 +0200

    Refactor Planning model and update PlannerAdvies page; rename properties, enhance data display, and remove PlannerDetail page

--- Models/Planning.cs
+++ Models/Planning.cs
@@ -2,12 +2,18 @@ namespace Sanibell_ProductionModule.Models;
 
 public sealed class Planning
 {
-    public required int AdviesId { get; init; }
-    public required string ArtikelOmschrijving { get; init; }
-    public required string ArtikelNummer { get; init; }
-    public required string Soort { get; init; }
-    public string? Status { get; set; } = "Nieuw advies";
-    public required int AanbevolenAantal { get; init; }
-    public required int HuidigAantal { get; init; }
-    public required int MaximaalAantal { get; init; }
+    public required int ArticleNumber { get; init; }
+    public required string ArticleDescription { get; init; }
+    public required string Size { get; init; }
+    public required string Color { get; init; }
+    public required int TotalCurrentStockNL { get; init; }
+    public required int TotalCurrentStockPL { get; init; }
+    public required int Recommended7Days { get; init; }
+    public required int Recommended14Days { get; init; }
+    public required int Recommended30Days { get; init; }
+    public required int MaxPossibleProduction { get; init; }
+    public required DateTime ReadyDate { get; init; }
+    public int? Amount { get; set; } // standaard = RecommendedAmount
+    public bool Urgency { get; set; } = false;
+    public bool Selection { get; set; } = false;
 }
--- Repositories/MockPlannerRepository.cs
+++ Repositories/MockPlannerRepository.cs
@@ -6,40 +6,26 @@ namespace Sanibell_ProductionModule.Repositories;
 public class MockPlannerRepository : IPlannerRepository
 {
     private static readonly IReadOnlyList<Planning> _plannings =
-        [
-        new Planning {AdviesId = 1, ArtikelNummer = "14-234B", ArtikelOmschrijving = "Wasbak", Status = "Nieuw advies", Soort = "Voorraadtekort", HuidigAantal = 0, AanbevolenAantal = 200, MaximaalAantal = 500 },
-        new Planning {AdviesId = 2, ArtikelNummer = "15-987A", ArtikelOmschrijving = "Kraan", Status = "Goedgekeurd", Soort = "Voorraadtekort", HuidigAantal = 50, AanbevolenAantal = 100, MaximaalAantal = 200 },
-        new Planning {AdviesId = 3, ArtikelNummer = "12-543C", ArtikelOmschrijving = "Toilet", Status = "In productie", Soort = "Seizoensvraag", HuidigAantal = 30, AanbevolenAantal = 80, MaximaalAantal = 150 },
-        new Planning {AdviesId = 4, ArtikelNummer = "18-765D", ArtikelOmschrijving = "Bad", Status = "Correctie gevraagd", Soort = "Voorraadtekort", HuidigAantal = 5, AanbevolenAantal = 20, MaximaalAantal = 50 },
-        new Planning {AdviesId = 5, ArtikelNummer = "11-234E", ArtikelOmschrijving = "Douchekop", Status = "Afgerond", Soort = "Nieuwe vraag", HuidigAantal = 60, AanbevolenAantal = 60, MaximaalAantal = 100 },
-        new Planning {AdviesId = 6, ArtikelNummer = "13-876F", ArtikelOmschrijving = "Spoelbak", Status = "Nieuw advies", Soort = "Voorraadtekort", HuidigAantal = 0, AanbevolenAantal = 150, MaximaalAantal = 300 },
-        new Planning {AdviesId = 7, ArtikelNummer = "17-432G", ArtikelOmschrijving = "Mengkraan", Status = "Goedgekeurd", Soort = "Seizoensvraag", HuidigAantal = 20, AanbevolenAantal = 60, MaximaalAantal = 120 },
-        new Planning {AdviesId = 8, ArtikelNummer = "16-555H", ArtikelOmschrijving = "Badmeubel", Status = "In productie", Soort = "Nieuwe vraag", HuidigAantal = 10, AanbevolenAantal = 40, MaximaalAantal = 80 },
-        new Planning {AdviesId = 9, ArtikelNummer = "19-321J", ArtikelOmschrijving = "Handdoekrek", Status = "Correctie gevraagd", Soort = "Voorraadtekort", HuidigAantal = 2, AanbevolenAantal = 15, MaximaalAantal = 30 },
-        new Planning {AdviesId = 10, ArtikelNummer = "20-654K", ArtikelOmschrijving = "WC-bril", Status = "Afgerond", Soort = "Seizoensvraag", HuidigAantal = 25, AanbevolenAantal = 25, MaximaalAantal = 50 },
-        new Planning {AdviesId = 11, ArtikelNummer = "21-111L", ArtikelOmschrijving = "Wastafel", Status = "Nieuw advies", Soort = "Voorraadtekort", HuidigAantal = 0, AanbevolenAantal = 100, MaximaalAantal = 200 },
-        new Planning {AdviesId = 12, ArtikelNummer = "22-222M", ArtikelOmschrijving = "Badkraan", Status = "Goedgekeurd", Soort = "Seizoensvraag", HuidigAantal = 40, AanbevolenAantal = 90, MaximaalAantal = 150 },
-        new Planning {AdviesId = 13, ArtikelNummer = "23-333N", ArtikelOmschrijving = "Doucheput", Status = "In productie", Soort = "Nieuwe vraag", HuidigAantal = 15, AanbevolenAantal = 60, MaximaalAantal = 120 },
-        new Planning {AdviesId = 14, ArtikelNummer = "24-444O", ArtikelOmschrijving = "Badkuip", Status = "Correctie gevraagd", Soort = "Voorraadtekort", HuidigAantal = 5, AanbevolenAantal = 25, MaximaalAantal = 50 },
-        new Planning {AdviesId = 15, ArtikelNummer = "25-555P", ArtikelOmschrijving = "Toiletbril", Status = "Afgerond", Soort = "Nieuwe vraag", HuidigAantal = 20, AanbevolenAantal = 20, MaximaalAantal = 40 },
-        new Planning {AdviesId = 16, ArtikelNummer = "26-666Q", ArtikelOmschrijving = "Handdouche", Status = "Nieuw advies", Soort = "Voorraadtekort", HuidigAantal = 0, AanbevolenAantal = 80, MaximaalAantal = 150 },
-        new Planning {AdviesId = 17, ArtikelNummer = "27-777R", ArtikelOmschrijving = "Wastafelkraan", Status = "Goedgekeurd", Soort = "Seizoensvraag", HuidigAantal = 30, AanbevolenAantal = 70, MaximaalAantal = 100 },
-        new Planning {AdviesId = 18, ArtikelNummer = "28-888S", ArtikelOmschrijving = "Badmeubelset", Status = "In productie", Soort = "Nieuwe vraag", HuidigAantal = 10, AanbevolenAantal = 50, MaximaalAantal = 100 },
-        new Planning {AdviesId = 19, ArtikelNummer = "29-999T", ArtikelOmschrijving = "Handdoekhouder", Status = "Correctie gevraagd", Soort = "Voorraadtekort", HuidigAantal = 3, AanbevolenAantal = 20, MaximaalAantal = 40 },
-        new Planning {AdviesId = 20, ArtikelNummer = "30-000U", ArtikelOmschrijving = "WC-rolhouder", Status = "Afgerond", Soort = "Seizoensvraag", HuidigAantal = 15, AanbevolenAantal = 15, MaximaalAantal = 30 },
-        new Planning {AdviesId = 21, ArtikelNummer = "31-111V", ArtikelOmschrijving = "Regendouche", Status = "Nieuw advies", Soort = "Nieuwe vraag", HuidigAantal = 0, AanbevolenAantal = 120, MaximaalAantal = 250 },
-        new Planning {AdviesId = 22, ArtikelNummer = "32-222W", ArtikelOmschrijving = "Badrand", Status = "Goedgekeurd", Soort = "Voorraadtekort", HuidigAantal = 25, AanbevolenAantal = 60, MaximaalAantal = 100 },
-        new Planning {AdviesId = 23, ArtikelNummer = "33-333X", ArtikelOmschrijving = "Douchewand", Status = "In productie", Soort = "Seizoensvraag", HuidigAantal = 8, AanbevolenAantal = 30, MaximaalAantal = 60 },
-        new Planning {AdviesId = 24, ArtikelNummer = "34-444Y", ArtikelOmschrijving = "Badfolie", Status = "Correctie gevraagd", Soort = "Nieuwe vraag", HuidigAantal = 2, AanbevolenAantal = 10, MaximaalAantal = 20 },
-        new Planning {AdviesId = 25, ArtikelNummer = "35-555Z", ArtikelOmschrijving = "Toiletpapierhouder", Status = "Afgerond", Soort = "Voorraadtekort", HuidigAantal = 12, AanbevolenAantal = 12, MaximaalAantal = 25 }
+    [
+        new Planning { ArticleNumber = 14234, ArticleDescription = "Wasbak", Size = "254x275", Color = "Red", TotalCurrentStockNL = 200, TotalCurrentStockPL = 50, Recommended7Days = 7, Recommended14Days = 12, Recommended30Days = 40, MaxPossibleProduction = 200, ReadyDate = DateTime.Today.AddDays(5) },
+        new Planning { ArticleNumber = 14235, ArticleDescription = "Kraan", Size = "Standaard", Color = "Chroom", TotalCurrentStockNL = 120, TotalCurrentStockPL = 80, Recommended7Days = 10, Recommended14Days = 16, Recommended30Days = 45, MaxPossibleProduction = 180, ReadyDate = DateTime.Today.AddDays(5) },
+        new Planning { ArticleNumber = 14236, ArticleDescription = "Toiletpot", Size = "Normaal", Color = "Wit", TotalCurrentStockNL = 60, TotalCurrentStockPL = 40, Recommended7Days = 8, Recommended14Days = 14, Recommended30Days = 35, MaxPossibleProduction = 100, ReadyDate = DateTime.Today.AddDays(6) },
+        new Planning { ArticleNumber = 14237, ArticleDescription = "Badkuip", Size = "160x70", Color = "Wit", TotalCurrentStockNL = 15, TotalCurrentStockPL = 10, Recommended7Days = 5, Recommended14Days = 8, Recommended30Days = 25, MaxPossibleProduction = 50, ReadyDate = DateTime.Today.AddDays(7) },
+        new Planning { ArticleNumber = 14238, ArticleDescription = "Douchewand", Size = "90x200", Color = "Helder Glas", TotalCurrentStockNL = 30, TotalCurrentStockPL = 20, Recommended7Days = 6, Recommended14Days = 9, Recommended30Days = 28, MaxPossibleProduction = 70, ReadyDate = DateTime.Today.AddDays(5) },
+        new Planning { ArticleNumber = 14239, ArticleDescription = "Spiegel", Size = "80x60", Color = "Zilver", TotalCurrentStockNL = 100, TotalCurrentStockPL = 60, Recommended7Days = 10, Recommended14Days = 15, Recommended30Days = 40, MaxPossibleProduction = 150, ReadyDate = DateTime.Today.AddDays(5) },
+        new Planning { ArticleNumber = 14240, ArticleDescription = "Handdoekrek", Size = "50cm", Color = "Zwart", TotalCurrentStockNL = 90, TotalCurrentStockPL = 45, Recommended7Days = 9, Recommended14Days = 13, Recommended30Days = 30, MaxPossibleProduction = 100, ReadyDate = DateTime.Today.AddDays(8) },
+        new Planning { ArticleNumber = 14241, ArticleDescription = "Zeepdispenser", Size = "Klein", Color = "Wit", TotalCurrentStockNL = 140, TotalCurrentStockPL = 70, Recommended7Days = 11, Recommended14Days = 18, Recommended30Days = 42, MaxPossibleProduction = 180, ReadyDate = DateTime.Today.AddDays(5) },
+        new Planning { ArticleNumber = 14242, ArticleDescription = "Toiletrolhouder", Size = "Standaard", Color = "Chroom", TotalCurrentStockNL = 180, TotalCurrentStockPL = 90, Recommended7Days = 12, Recommended14Days = 20, Recommended30Days = 48, MaxPossibleProduction = 200, ReadyDate = DateTime.Today.AddDays(5) },
+        new Planning { ArticleNumber = 14243, ArticleDescription = "Douchestang", Size = "70cm", Color = "Zilver", TotalCurrentStockNL = 80, TotalCurrentStockPL = 30, Recommended7Days = 8, Recommended14Days = 14, Recommended30Days = 36, MaxPossibleProduction = 100, ReadyDate = DateTime.Today.AddDays(6) },
     ];
+
 
     public Task<IReadOnlyList<Planning>> GetPlanningAsync(CancellationToken ct = default)
         => Task.FromResult(_plannings);
 
     public Task<Planning?> GetByIdAsync(int id, CancellationToken ct = default)
     {
-        var plannings = _plannings.FirstOrDefault(u => u.AdviesId == id);
+        var plannings = _plannings.FirstOrDefault(u => u.ArticleNumber == id);
         return Task.FromResult(plannings);
     }
 }
