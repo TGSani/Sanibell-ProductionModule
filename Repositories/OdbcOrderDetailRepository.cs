4c24f4baf3eeb1cb810e00b49851898b76ef9f37
Thomas G <thomas.geeve@sanibell.nl>
2025-11-11 13:58:22 +0100

    Enhance SQL query in OdbcOrderDetailRepository for improved clarity and maintainability

--- Repositories/OdbcOrderDetailRepository.cs
+++ Repositories/OdbcOrderDetailRepository.cs
@@ -23,7 +23,46 @@ public class OdbcOrderDetailRepository : IOrderDetailRepository
     }
 
     private static readonly string GetByIdSql = """ 
+        select   RcptCode AS ReceptCode
+        ,RcpeRegelNr
+        ,RcpeAantal AS Aantal
+        ,ArtCode AS OrderNummer
+        ,ArtOms
+        ,VRART_Kleur 
+        ,VRART_Maat
+        ,ArtEANCode AS EANCode
 
+        From      KingSystem.tabReceptuur
+          inner join KingSystem.tabReceptuurEindproduct  ON RcpeRcptGid = RcptGid
+          
+          left join KingSystem.VrGetContent('RCP',0,0,'Opnemen in productie app','','')
+                                        WITH(RCP_gid integer
+                                            ,RCP_Opnemen integer
+                                            ) RCP ON RCP_gid = RcptGid
+          
+          
+           inner join KingSystem.tabArtikel on RcpeArtGid =  ArtGid
+            left join KingSystem.VrGetContent('ART',0,0,
+                                              'Kleur
+                                               Maat
+                                              ','','')
+                                          WIth(VRART_gid integer
+                                             ,VRART_Kleur nchar(20)
+                                             ,VRART_Maat Nchar(20)
+                                             ) VRART ON VRART_Gid = ArtGid
+           left join KingSystem.tabArtikelEan on ArtGid = ArtEanArtGid
+          
+          
+           
+        /* voorbeeld om per etiket een regel te maken  */ 
+        --   left join rowgenerator on row_num <= RcpeAantal
+           
+          
+        Where       isnull(RCP_Opnemen,0) = 1
+            AND ArtEanIsDefault = 1
+
+        Order by     RcpeRegelNr
+          
         """;
 
     public async Task<IReadOnlyList<OrderDetail>> GetDetailByIdAsync(int id, CancellationToken ct = default)
