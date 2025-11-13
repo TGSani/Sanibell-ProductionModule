f4a3959cab64fe0bdeadd8df36343f1c10016a17
Thomas G <thomas.geeve@sanibell.nl>
2025-11-13 09:05:26 +0100

    Add shadowbox styling to buttons and update service registration for planner repository

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped<MenuTileService>();
 builder.Services.AddScoped<PlannerErpService>();
 builder.Services.AddScoped<IOrderRepository, MockOrderRepository>(); // switch between "MockOrderRepository" and "OdbcOrderRepository" here
 builder.Services.AddScoped<IOrderDetailRepository, MockOrderDetailRepository>(); // switch between "MockOrderDetailRepository" and "OdbcOrderDetailRepository" here
-builder.Services.AddScoped<IPlannerRepository, OdbcPlannerRepository>(); // switch between "MockPlannerRepository" and "OdbcPlannerRepository" here
+builder.Services.AddScoped<IPlannerRepository, MockPlannerRepository>(); // switch between "MockPlannerRepository" and "OdbcPlannerRepository" here
 builder.Services.AddScoped<IBarCodeGenService, BarCodeGenService>();
 
 
